d5d3516215f6d4654abb30c9df0d29956287a810
Oleksandr Omelkovets <omelkovets69@gmail.com>
2023-10-30 18:41:20 +0200

    fix

--- routes/api.php
+++ routes/api.php
@@ -59,7 +59,7 @@ Route::group(['prefix' => 'subscriptions'], function () {
 Route::get('/animals', [AnimalController::class, 'index']);
 
 
-Route::group(['prefix' => 'categories'], function () {
+Route::group(['prefix' => 'subcategories'], function () {
     Route::get('/', [CategoryController::class, 'index']);
     Route::get('/{category}/subcategories', [CategoryController::class, 'subcategories']);
     Route::get('/{category}/products', [CategoryController::class, 'products']);
